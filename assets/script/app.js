845170d6618a18aad70ab1d45861f91b0c86c4d6
Emmanuel Olure <emmanuelolabayode@gmail.com>
2025-12-10 22:38:33 -0600

    log in authentication and redirect to home page

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+localStorage.setItem('username', 'tester');
+localStorage.setItem('password', 'test1234');
+
+
+const loginForm = document.getElementById('login-form');
+const errorMessage = document.getElementById('errorMessage');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const inputUsername = username.value.trim();
+    const inputPassword = password.value.trim();
+
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+    if(inputUsername === storedUsername && inputPassword === storedPassword){
+        localStorage.setItem('isLoggedIn', 'true')
+        
+        window.location.href = 'home.html'
+    } else {
+        errorMessage.textContent = 'Incorrect Credentials'
+    }
+})
