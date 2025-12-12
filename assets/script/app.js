467c1e914c6956e254eaa3bba1a722f07e101253
Emmanuel Olure <emmanuelolabayode@gmail.com>
2025-12-11 18:29:12 -0600

    split js files to reduce load time on login page

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,40 +1,20 @@
 'use strict';
 
+if (!localStorage.getItem('isLoggedIn') || localStorage.getItem('isLoggedIn') !== 'true') {
+    window.location.href = 'index.html';
+}
 
-const loginForm = document.getElementById('login-form');
 const peopleGrid = document.getElementById('peopleGrid');
 const profilePic = document.getElementById('profile-pic');
 const modal = document.getElementById('settingsDialog');
 const closeBtn = document.getElementById('closeBtn');
+const signOut = document.getElementById('btnTwo');
 const URL = 'https://randomuser.me/api/?results=10&inc=picture,name,location&seed=same';
 
 // fetch(URL).then(r=>r.json()).then(console.log);
 
 const options = {
     method: 'GET',
-}
-if (loginForm) {
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
-    const errorMessage = document.getElementById('errorMessage');
-    
-    localStorage.setItem('username', 'tester');
-    localStorage.setItem('password', 'test1234');
-    
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const inputUsername = username.value.trim();
-        const inputPassword = password.value.trim();
-        const storedUsername = localStorage.getItem('username');
-        const storedPassword = localStorage.getItem('password');
-        
-        if (inputUsername === storedUsername && inputPassword === storedPassword) {
-            localStorage.setItem('isLoggedIn', 'true');
-            window.location.href = 'home.html';
-        } else {
-            errorMessage.textContent = 'Incorrect Username or Password';
-        }
-    });
 }
 
 
@@ -81,6 +61,17 @@ profilePic.addEventListener('click', ()=> {
 
 closeBtn.addEventListener('click', ()=> {
     modal.close();
+})
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) { 
+        modal.close();
+    }
+})
+
+signOut.addEventListener('click', () => {
+    localStorage.setItem('isLoggedIn', 'false');
+    window.location.href = 'index.html';
 })
 
 
